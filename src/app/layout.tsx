1953ea908e4bd9000484a5b6e46672e115bcc43c
Kevin Kariuki <kevinkaris47@gmail.com>
2026-01-07 13:27:00 +0000

    Instead of adding another icon in src/app, we could use the favicon in t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export const metadata: Metadata = {
   description: 'Financial management for your livestock enterprise.',
   manifest: '/manifest.json',
   icons: {
-    icon: '/icon.png',
-    apple: '/icon.png',
+    icon: '/favicon.ico', // This will now correctly point to src/app/favicon.ico
+    apple: '/images/icon-192x192.png',
   },
 };
 
